c51ce5872caf50b5622bdd9e9764f62ba6629823
Florencia Santucho <andreaf.santucho@gmail.com>
2025-05-29 22:46:19 -0300

    Agrego ruta en app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+import mocksRouter from './routes/mocks.router.js';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -15,11 +16,13 @@ const PORT = process.env.PORT||8080;
 mongoose.connect(process.env.MONGO_URL);
 
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
 app.use('/api/adoptions',adoptionsRouter);
 app.use('/api/sessions',sessionsRouter);
+app.use('/api/mocks', mocksRouter);
 
 app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
